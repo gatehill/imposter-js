bed77c51e56020ebe29c015b72453c3a8a239c35
Pete Cornish <outofcoffee@gmail.com>
2022-01-05 09:55:10 +0000

    fix: environment variable name validation.

--- src/configured-mock.js
+++ src/configured-mock.js
@@ -108,7 +108,7 @@ export class ConfiguredMock {
 
     validateEnv(env) {
         for (const key in env) {
-            if (!key.match(/IMPOSTER_.+/) || key !== "JAVA_TOOL_OPTIONS") {
+            if (!key.match(/IMPOSTER_.+/) && key !== "JAVA_TOOL_OPTIONS") {
                 nodeConsole.warn(`Environment variable ${key} does not match IMPOSTER_* or JAVA_TOOL_OPTIONS - this may be ignored by the mock engine`);
             }
         }
